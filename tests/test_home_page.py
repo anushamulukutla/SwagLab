c0208edc2fa980d60db1e8e169f2798cce03a744
Mahsa <mahsamanouchehrii@gmail.com>
2026-02-19 20:30:48 -0500

    updated the test_login_with_standard_user

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -17,22 +17,4 @@ class TestLoginPage:
         assert login_page.is_page_loaded()
        # assert.is_page_loaded()
 
-    class TestLogin:
 
-        @pytest.mark.smoke
-        def test_TC_LGN_001_valid_login_standard_user(self, login_page, driver):
-
-            login_page.open()
-            assert login_page.is_page_loaded(), "Login page did not load"
-
-            login_page.login("standard_user", "secret_sauce")
-            time.sleep(3)
-
-            wait = WebDriverWait(driver, 10)
-
-            wait.until(EC.url_contains("/inventory.html"))
-            assert "/inventory.html" in driver.current_url, "Not redirected to /inventory.html"
-
-            products_title = (By.CLASS_NAME, "title")  # SauceDemo shows "Products"
-            title_el = wait.until(EC.visibility_of_element_located(products_title))
-            assert title_el.text.strip() == "Products", "Products page not displayed"
